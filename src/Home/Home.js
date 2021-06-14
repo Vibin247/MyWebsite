2f967cc19c55f342b9dfead142f801348a154327
vibin.chakravarthy <vibin.chakravarthy@zohocorp.com>
2021-06-15 00:57:43 +0530

    Image location fixes.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,9 @@
-// import MyFace from "./../MyFace.jpeg";
 import HomeInfo from "./HomeInfo";
 
 export default function Home(){
     return (
         <div className="HomePage">
-            <HomeInfo src="/MyFace.jpeg"/>
+            <HomeInfo src="./images/MyFace.png"/>
             {/* <div className="ScrollDownWrapper">
                 <div className="ScrollDownRow">
                     <div className="ScrollButton"></div>
