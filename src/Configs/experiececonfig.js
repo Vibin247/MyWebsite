b477846d3be34d43a94d53116e2dacee7a387695
vibin.chakravarthy <vibin.chakravarthy@zohocorp.com>
2022-01-14 14:41:58 +0530

    Add LSN experience.

--- src/Configs/experiececonfig.js
+++ src/Configs/experiececonfig.js
@@ -1,20 +1,26 @@
-import MDM from "../Components/Professional/Experiences/MDM";
-import ARW from "../Components/Professional/Experiences/ARW";
+import Experiences from "../Components/Professional/Experiences"
 
 const experiececonfig = [
     {
         duration:"June 2018 - October 2020",
         designation: "Member Technical Staff, Mobile Device Manager, Zoho Corporation",
-        descriptionComponent: MDM,
+        descriptionComponent: Experiences.MDM,
         current: false,
         name:"mdm"
     },
     {
-        duration:"November 2020 - Present",
+        duration:"November 2020 - October 2021",
         designation: "Member Technical Staff, Anti-Ransomware, Zoho Corporation",
-        descriptionComponent: ARW,
-        current: true,
+        descriptionComponent: Experiences.ARW,
+        current: false,
         name:"arw"
+    },
+    {
+        duration:"October 2021 - Present",
+        designation: "Software Development Engineer 2, Loadshare Networks Ltd.",
+        descriptionComponent: Experiences.LSN,
+        current: true,
+        name:"lsn"
     }
 ];
 
