5784c069183df80887d2cc117d7b19315571d0fd
Vibin Chakravarthy <11892837+vibin247@users.noreply.github.com>
2022-09-02 13:02:57 +0530

    Update Welcome.js

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome(props){
                 <img src={props.src} alt="Vibin" width="200px" height="200px"/>
             </div>
             <div className="description-tag disable-text-select">
-                <div className="greeting">Hi, I am Vibin Chakravarthy.</div>
+                <div className="greeting">Hi, I am Vibin Chakravarthy. And if you are not me, you are not supposed to see this. </div>
                 <div>A professional web developer looking to lend my expertise to a technical product that is a driving force in today's software world. </div>
             </div>
         </div>
